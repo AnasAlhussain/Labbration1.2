1ab91b2de228a47935f85fa8342789eb2c16030d
Anas Alhussain <anasalhussain.sweeden@hotmail.com>
2024-10-03 21:37:45 +0200

    Add project files.

---
+++ Entiteter/Kurs.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schemssystem_modell
+{
+    public class Kurs
+    {
+        public string KursNamn { get; set; }
+
+        public string Akronym { get; set; }
+
+
+        public Schema Schema { get; set; }
+
+        public List<KursTillfälle> KursTillfälles { get; set; }
+        public Utibildning Utibildning { get; set; }
+
+
+        //public Kurs(string kursnamn, string akronym)
+        //{
+        //    this.KursNamn = kursnamn;
+
+
+        //    this.Akronym = akronym;
+
+
+
+        //}
+
+
+    }
+}
---
+++ Entiteter/Lokal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schemssystem_modell
+{
+    public class Lokal
+    {
+        public string LokalNummer { get; set; }
+        public int Plaster { get; set; }
+
+        //public Lokal(string lokNum, int anatalplats)
+        //{
+        //    this.LokalNummer = lokNum;
+        //    this.Plaster = anatalplats;
+        //}
+    }
+}
